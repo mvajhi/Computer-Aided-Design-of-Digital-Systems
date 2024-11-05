8546239e034523c45a5a4b946d9de76bd64da1cd
mvajhi <mvajhimv@gmail.com>
2024-11-05 21:53:33 +0330

    Fix param name

--- ca2/src/hdl/datapath.v
+++ ca2/src/hdl/datapath.v
@@ -1,18 +1,20 @@
-module datapath #(parameter WIDTH = 8, parameter DEPTH = 16, parameter PAR_WRITE = 1, parameter PAR_READ = 1) (
+module datapath #(parameter DATA_WIDTH = 8, parameter SIZE = 16, parameter PAR_WRITE = 1, parameter PAR_READ = 1) (
     input wire clk,
     input wire rst,
-    input wire [WIDTH-1:0] data_in [PAR_WRITE-1:0],
+    input wire [DATA_WIDTH-1:0] data_in [PAR_WRITE-1:0],
     input wire write_enable,
     input wire read_enable,
-    output reg [WIDTH-1:0] data_out [PAR_READ-1:0],
+    output reg [DATA_WIDTH-1:0] data_out [PAR_READ-1:0],
     output wire full,
     output wire empty
 );
-    input wire [$clog2(DEPTH)-1:0] w_address;
-    input wire [$clog2(DEPTH)-1:0] r_address;
-    input wire [$clog2(DEPTH)-1:0] len;
+    localparam SEL_WIDTH = $clog2(SIZE);
 
-    up_down_counter #(.WIDTH(WIDTH), .UP_STEP(PAR_WRITE), .DOWN_STEP(PAR_READ)) len_counter (
+    wire [SEL_WIDTH-1:0] w_address;
+    wire [SEL_WIDTH-1:0] r_address;
+    wire [SEL_WIDTH-1:0] len;
+
+    up_down_counter #(.WIDTH(SEL_WIDTH), .UP_STEP(PAR_WRITE), .DOWN_STEP(PAR_READ)) len_counter (
         .clk(clk),
         .rst(rst),
         .up_enable(write_enable),
@@ -20,35 +22,35 @@ module datapath #(parameter WIDTH = 8, parameter DEPTH = 16, parameter PAR_WRITE
         .count(len)
     );
 
-    configurable_counter #(.WIDTH(WIDTH), .STEP(PAR_WRITE)) write_counter (
+    configurable_counter #(.WIDTH(SEL_WIDTH), .STEP(PAR_WRITE)) write_counter (
         .clk(clk),
         .rst(rst),
         .enable(write_enable),
         .count(w_address)
     );
 
-    configurable_counter #(.WIDTH(WIDTH), .STEP(PAR_READ)) read_counter (
+    configurable_counter #(.WIDTH(SEL_WIDTH), .STEP(PAR_READ)) read_counter (
         .clk(clk),
         .rst(rst),
         .enable(read_enable),
         .count(r_address)
     );
 
-    wire [$clog2(DEPTH)-1:0] len_add;
+    wire [$clog2(SIZE)-1:0] len_add;
     wire len_add_co;
 
     assign {len_add_co, len_add} = len + PAR_WRITE;
     assign full = len_add_co;
 
-    wire [$clog2(DEPTH):0] len_sub;
+    wire [$clog2(SIZE):0] len_sub;
     assign len_sub = {0, len} - (PAR_READ + 1);
-    assign empty = len_sub[$clog2(DEPTH)];
+    assign empty = len_sub[$clog2(SIZE)];
 
     instant_buffer #(
-        .SIZE(DEPTH),
+        .SIZE(SIZE),
         .WRITE_SIZE(PAR_WRITE),
         .READ_SIZE(PAR_READ),
-        .DATA_WIDTH(WIDTH)
+        .DATA_WIDTH(DATA_WIDTH)
     ) circular_buffer (
         .in(data_in),
         .write_addr(w_address),
