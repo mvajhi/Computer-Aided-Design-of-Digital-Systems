fab71f9e3851b51c07d18f6777639ba13430f535
mvajhi <mvajhimv@gmail.com>
2024-11-07 00:25:31 +0330

    Restore tb

--- ca2/sim/tb/tb.sv
+++ ca2/sim/tb/tb.sv
@@ -3,7 +3,7 @@ module tb ();
     reg rst = 0;
     reg [7:0] data_in [3:0];
     reg write_enable = 0;
-    reg read_enable = 0;
+    reg read_enable = 4'b0100;
     wire [7:0] data_out [3:0];
     wire full;
     wire empty;
@@ -11,124 +11,32 @@ module tb ();
     wire ready;
 
     initial begin
-        #10 write_enable = 1;
-        data_in[0] = 8'd1;
-        data_in[1] = 8'd2;
-        data_in[2] = 8'd3;
-        data_in[3] = 8'd4;
+        rst = 1;
+        #10 rst = 0;
 
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = 8'd8;
-        data_in[1] = 8'd7;
-        data_in[2] = 8'd6;
-        data_in[3] = 8'd5;
+        data_in[0] = 8'b10101010;
+        data_in[1] = 8'b11111111;
+        data_in[2] = 8'b00000000;
+        data_in[3] = 8'b01010101;
         
         #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = 8'd10;
-        data_in[1] = 8'd14;
-        data_in[2] = 8'd16;
-        data_in[3] = 8'd19;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = 8'd10;
-        data_in[1] = 8'd14;
-        data_in[2] = 8'd16;
-        data_in[3] = 8'd19;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = 8'd10;
-        data_in[1] = 8'd14;
-        data_in[2] = 8'd16;
-        data_in[3] = 8'd19;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = 8'd10;
-        data_in[1] = 8'd14;
-        data_in[2] = 8'd16;
-        data_in[3] = 8'd19;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = -1;
-        data_in[1] = -2;
-        data_in[2] = -3;
-        data_in[3] = -4;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = -1;
-        data_in[1] = -2;
-        data_in[2] = -3;
-        data_in[3] = -4;
-
-        #10 write_enable = 1;
-        read_enable = 1;
-        data_in[0] = -1;
-        data_in[1] = -2;
-        data_in[2] = -3;
-        data_in[3] = -4;
-
-        #10 write_enable = 1;
-        read_enable = 0;
-        data_in[0] = -1;
-        data_in[1] = -2;
-        data_in[2] = -3;
-        data_in[3] = -4;
-
         #10 write_enable = 0;
-        read_enable = 0;
-        data_in[0] = -1;
-        data_in[1] = -2;
-        data_in[2] = -3;
-        data_in[3] = -4;
-
+        #30 write_enable = 1;
         #10 write_enable = 0;
-        read_enable = 1;
-
-        #10 write_enable = 0;
-        read_enable = 1;
-
-        #10 write_enable = 0;
-        read_enable = 1;
-
-        #10 write_enable = 0;
-        read_enable = 1;
-
-
+        #10 read_enable = 1;
+        #10 read_enable = 0;
+        #30 read_enable = 1;
+        #10 read_enable = 0;
+        
+        #1000 $finish;
     end
-
-    // initial begin
-    //     rst = 1;
-    //     #10 rst = 0;
-
-    //     data_in[0] = 8'b10101010;
-    //     data_in[1] = 8'b11111111;
-    //     data_in[2] = 8'b00000000;
-    //     data_in[3] = 8'b01010101;
-        
-    //     #10 write_enable = 1;
-    //     #10 write_enable = 0;
-    //     #30 write_enable = 1;
-    //     #10 write_enable = 0;
-    //     #10 read_enable = 1;
-    //     #10 read_enable = 0;
-    //     #30 read_enable = 1;
-    //     #10 read_enable = 0;
-        
-    //     #1000 $finish;
-    // end
     always #5 clk = ~clk;
 
     fifo_buffer #(
         .DATA_WIDTH(8),
         .SIZE(8),
         .PAR_WRITE(4),
-        .PAR_READ(1)
+        .PAR_READ(4)
     )
     dp (
         .clk(clk),
