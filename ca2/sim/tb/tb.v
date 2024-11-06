fe6f3f0c9934bc38fc7a71b8ba0e38ac86c2d6d6
lrz <lrz82mirshafee@gmail.com>
2024-11-06 16:29:08 +0330

    Update tb.v

--- ca2/sim/tb/tb.v
+++ ca2/sim/tb/tb.v
@@ -1,23 +1,50 @@
 module tb ();
     reg clk = 0;
     reg rst = 0;
-    reg start = 0;
-    wire done;
+    wire [7:0] data_in [3:0]= 0;
+    wire write_enable = 0;
+    wire read_enable = 0;
+    wire [7:0] data_out [3:0];
+    wire full;
+    wire empty;
+    wire valid;
+    wire ready;
 
     initial begin
         rst = 1;
         #10 rst = 0;
-        #10 start = 1;
-        #10 start = 0;
+
+        data_in[0] = 8'b10101010;
+        data_in[1] = 8'b11111111;
+        data_in[2] = 8'b00000000;
+        data_in[3] = 8'b01010101;
         
-        #3000 $finish;
+        #10 write_enable = 1;
+        #10 write_enable = 0;
+        #10 read_enable = 1;
+        #10 read_enable = 0;
+        
+        #1000 $finish;
     end
     always #5 clk = ~clk;
 
-    top_module dut (
+    fifo_buffer #(
+        .DATA_WIDTH(8),
+        .SIZE(8),
+        .PAR_WRITE(4),
+        .PAR_READ(4)
+    )
+    dp (
         .clk(clk),
-        .rst(rst),  
-        .start(start),
-        .done(done)
-    );
+        .rst(rst),
+        .data_in(data_in),
+        .write_enable(write_enable),
+        .read_enable(read_enable),
+        .data_out(data_out),
+        .full(full),
+        .empty(empty),
+        .valid(valid),
+        .ready(ready)
+    )
+
 endmodule
