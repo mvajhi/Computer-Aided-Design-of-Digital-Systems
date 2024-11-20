4189b3c8b1671c50ffd040b530bcf3a232377c50
mvajhi <mvajhimv@gmail.com>
2024-11-20 11:32:58 +0330

    add rst

--- mid/src/hdl/dp_reg.v
+++ mid/src/hdl/dp_reg.v
@@ -10,10 +10,17 @@ module dp_reg (
     output reg [31:0] out_sum,
     output reg        out_overflow
 );
-    always @(posedge clk) begin
-        out_x <= in_x;
-        out_num <= in_num;
-        out_sum <= in_sum;
-        out_overflow <= in_overflow;
+    always @(posedge clk, posedge rst) begin
+        if (rst) begin
+            out_x <= 32'b0;
+            out_num <= 32'b0;
+            out_sum <= 32'b0;
+            out_overflow <= 1'b0;
+        end else begin
+            out_x <= in_x;
+            out_num <= in_num;
+            out_sum <= in_sum;
+            out_overflow <= in_overflow;
+        end
     end
 endmodule
