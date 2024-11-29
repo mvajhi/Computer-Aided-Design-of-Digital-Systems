5330cb80dd917b768261768b2f1333a11cecdfb8
lrz <lrz82mirshafee@gmail.com>
2024-11-29 14:43:12 +0330

    Update counter_dual.v

--- ca3/src/hdl/counter_dual.v
+++ ca3/src/hdl/counter_dual.v
@@ -12,7 +12,7 @@ module Counter_dual #(
     
     adder #(
         .WIDTH(WIDTH)
-    ) adder_inst_1( .in1(out), .in2(en1 || en2), .cin(en2), .out(adder_out));
+    ) adder_inst_1( .in1(out), .in2(en1 || en2 || en_d), .cin(en2), .out(adder_out));
 
     wire [WIDTH-1:0] adder_out;
     reg [WIDTH-1:0] out;
