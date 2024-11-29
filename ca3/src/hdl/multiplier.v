278c441315963387b4a74ef63a1d5a6db5a8fd05
mvajhi <mvajhimv@gmail.com>
2024-11-29 19:18:11 +0330

    write bit mul

--- ca3/src/hdl/multiplier.v
+++ ca3/src/hdl/multiplier.v
@@ -7,3 +7,40 @@ module multiplier #(
 );
     assign out = in1 * in2;
 endmodule
+
+module bit_multiplier
+(
+    input xi, yi, ci, pi,
+    output xo, yo, co, po
+);
+    wire xy;
+    assign xo = xi;
+    assign yo = yi;
+
+    and_gate and_inst (
+        .a(xi),
+        .b(yi),
+        .y(xy)
+    );
+    assign co = (pi & xy) | (pi & ci) | (xy & ci);
+    assign po = pi ^ ci ^ xy;
+
+endmodule
+
+module and_gate
+(
+    input a, b,
+    output y
+);
+    C1 and_inst (
+        .A0(1'b0),
+        .A1(b),
+        .SA(a),
+        .B0(1'b0),
+        .B1(1'b0),
+        .SB(1'b0),
+        .S0(1'b0),
+        .S1(1'b0),
+        .F(y)
+    );
+endmodule
