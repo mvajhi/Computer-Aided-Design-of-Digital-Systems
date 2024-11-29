c1fba78a4a9e8b0717347733495a7b59a717f55e
mvajhi <mvajhimv@gmail.com>
2024-11-29 15:28:16 +0330

    Added multiplexer

--- ca3/src/hdl/mux.v
+++ ca3/src/hdl/mux.v
@@ -7,6 +7,21 @@ module multiplexer #(
     output wire [WIDTH-1:0] out
 );
 
-    assign out = sel ? in1 : in0;
+genvar i;
+generate
+    for (i = 0; i < WIDTH; i = i + 1) begin : mux_block
+        C1 mux_inst (
+            .A0(in0[i]),
+            .A1(in1[i]),
+            .SA(sel),
+            .B0(1'b1),
+            .B1(1'b1),
+            .SB(1'b1),
+            .S0(1'b1),
+            .S1(1'b1),
+            .F(out[i])
+        );
+    end
+endgenerate
 
 endmodule
