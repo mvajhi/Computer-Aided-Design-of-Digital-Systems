03bddfa11a0f2a2a5f1f94d9fcce0c4e87287f8f
mvajhi <mvajhimv@gmail.com>
2024-11-29 23:42:28 +0330

    Fix counter

--- ca3/src/hdl/counter.v
+++ ca3/src/hdl/counter.v
@@ -9,21 +9,43 @@ module Counter #(
     output wire co
 );
     wire [WIDTH-1:0] adder_out;
-    reg [WIDTH-1:0] counter;
+    wire [WIDTH-1:0] counter;
+    wire co_add;
 
-    adder #(.WIDTH(WIDTH)) adder_inst(.in1(counter), .in2(3'b001), .cin(1'b0), .out(adder_out));
+    adder #(.WIDTH(WIDTH)) adder_inst(.in1(counter), .in2(3'b000), .cin(en), .out(adder_out), .co(co));
 
 
-    always @(posedge clk or posedge rst) begin
-        if(rst)
-            counter <= 0;
-        else if(load)
-            counter <= in;
-        else if(en)
-            counter <= adder_out;
-    end
+    // always @(posedge clk or posedge rst) begin
+    //     if(rst)
+    //         counter <= 0;
+    //     else if(load)
+    //         counter <= in;
+    //     else if(en)
+    //         counter <= adder_out;
+    // end
 
-    assign co = &counter && en;
+    genvar i;
+
+    generate
+        for (i = 0; i < WIDTH; i = i + 1) begin : register_block
+            // data[i] sel if load and shift_en is 0
+            // data[i-1] sel if load is 0 and shift_en is 1
+            // in[i] sel if load is 1
+            S2 reg_inst (
+                .A0(en),
+                .B0(en),
+                .A1(load),
+                .B1(load),
+                .D({in[i], in[i], adder_out[i], counter[i]}),
+                .CLK(clk),
+                .CLR(rst),
+                .out(counter[i])
+            );
+        end
+    endgenerate
+
+    // assign co = &counter && en;
+    // and_mod and_co (co_add, en, co);
 
 endmodule
 
--- ca3/src/hdl/counter_dual.v
+++ ca3/src/hdl/counter_dual.v
@@ -10,28 +10,60 @@ module Counter_dual #(
     output co
 );
     wire [WIDTH-1:0] adder_out;
-    reg [WIDTH-1:0] out;
+    wire [WIDTH-1:0] out;
+    wire co_add;
     
     wire temp;
-    assign temp = (en1 | en2) | en_d;
+    // assign temp = (en1 | en2) | en_d;
+    C1 c1_inst_en1 (
+        .A0(1'b0),
+        .A1(1'b1),
+        .SA(en_d),
+        .B0(1'b1),
+        .B1(1'b1),
+        .SB(1'b1),  
+        .S0(en1),
+        .S1(en2),
+        .F(temp)
+    );
 
     adder #(
         .WIDTH(WIDTH)
-    ) adder_inst_1( .in1(out), .in2({3'b0, temp}), .cin(en2), .out(adder_out));
+    ) adder_inst_1( .in1(out), .in2({3'b0, temp}), .cin(en2), .out(adder_out), .co(co_add) );
 
 
-    always @(posedge clk or posedge rst) begin
-        if(rst)
-            out <= {WIDTH{1'b0}};
-        else if(en_d)
-            out <= adder_out;
-        else if(en2)
-            out <= adder_out;
-        else if(en1)
-            out <= adder_out;
-    end
+    // always @(posedge clk or posedge rst) begin
+    //     if(rst)
+    //         out <= {WIDTH{1'b0}};
+    //     else if(en_d)
+    //         out <= adder_out;
+    //     else if(en2)
+    //         out <= adder_out;
+    //     else if(en1)
+    //         out <= adder_out;
+    // end
 
-    assign co = &out;
+    ShiftRegister #(
+        .WIDTH(WIDTH)
+    ) sr_inst (
+        .clk(clk),
+        .rst(rst),
+        .load(1'b1),
+        .shift_en(1'b0),
+        .in(adder_out),
+        .in_sh(1'b0),
+        .out(out)
+    );
+
+    // assign co = &out;
+    C2 co_calc(
+        .A0(out[0]),
+        .B0(out[1]),
+        .A1(out[2]),
+        .B1(out[2]),
+        .D({out[3], 3'b0}),
+        .out(co)
+    );
 endmodule
 
 
