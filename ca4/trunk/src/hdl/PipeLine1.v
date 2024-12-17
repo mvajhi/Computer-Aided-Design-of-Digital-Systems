8ca33255d1cef6949bbabb742922fcb767f949de
lrz <lrz82mirshafee@gmail.com>
2024-12-17 20:40:40 +0330

    Update PipeLine1.v

--- ca4/trunk/src/hdl/PipeLine1.v
+++ ca4/trunk/src/hdl/PipeLine1.v
@@ -1,1 +1,13 @@
-Pip
+module Pipeline1 #(
+    parameter DATA_WIDTH = 8
+) (
+    input  wire clk,
+    input  wire rst,
+    input  wire stall,
+    input  wire [DATA_WIDTH-1:0] in,
+    output reg  [DATA_WIDTH-1:0] out,
+    output reg  stall_out,
+);
+
+    
+endmodule
