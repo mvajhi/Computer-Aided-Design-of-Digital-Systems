bdd76282c6e9aae496f0f0f377361eac8702a440
mvajhi <mvajhimv@gmail.com>
2024-12-18 17:13:11 +0330

    Added len_check

---
+++ ca4/src/hdl/len_check.v
@@ -0,0 +1,25 @@
+module len_check #(parameter WIDTH = 4) (
+    input wire clk,
+    input wire rst,
+    input wire up_enable,
+    input wire down_enable,
+    output empty,
+    output full
+);
+
+    reg [WIDTH-1:0] count;
+    wire [WIDTH-1:0] count_inc = up_enable ? count + 1'b1 : count;
+    wire [WIDTH-1:0] count_dec = down_enable ? count_inc - 1b'1 : count_inc;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            count <= 0;
+        end else begin
+            count <= count_dec;
+        end
+    end
+
+    assign empty = (count == 0);
+    assign full = (count == (2**WIDTH - 1));
+
+endmodule
