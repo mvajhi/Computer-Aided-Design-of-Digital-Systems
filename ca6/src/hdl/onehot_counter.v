867d287bc58c992dfa57da9cc6030ebb52b15b42
mvajhi <mvajhimv@gmail.com>
2025-02-10 23:54:47 +0330

    Added co

--- ca6/src/hdl/onehot_counter.v
+++ ca6/src/hdl/onehot_counter.v
@@ -3,16 +3,20 @@ module OneHotCounter #(parameter WIDTH = 8) (
     input  rst,
     input  enable,
     input  count,
-    output reg [WIDTH-1:0] one_hot
+    output reg [WIDTH-1:0] one_hot,
+    output reg co
 );
 
     always @(posedge clk or posedge rst) begin
+        co = 1'b0;
         if (reset)
             one_hot <= 1;
         else if (!enable)
             one_hot <= 0;
-        else if (count)
+        else if (count) begin
             one_hot <= {one_hot[WIDTH-2:0], one_hot[WIDTH-1]};
+            co = 1'b1;
+        end
     end
     
 endmodule
