70c38153f5f100587158fa85dfbcda2553c41816
mvajhi <mvajhimv@gmail.com>
2025-02-10 21:01:14 +0330

    Added addr gen

---
+++ ca6/src/hdl/Addr_counter.v
@@ -0,0 +1,19 @@
+module AddressCounter #(parameter ADDR_WIDTH = 8, parameter OFFSET = 0) (
+    input   clk,
+    input   reset,
+    input   enable,
+    output reg [ADDR_WIDTH-1:0] addr
+);
+
+    logic [ADDR_WIDTH-1:0] counter;
+    
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset)
+            counter <= 0;
+        else if (enable)
+            counter <= counter + 1;
+    end
+    
+    assign addr = counter + OFFSET;
+    
+endmodule
